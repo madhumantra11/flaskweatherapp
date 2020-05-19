d02b5260d06a68cd88223b7cd22ebb0d3d054f29
Madhulata Baid <58360275+madhumantra11@users.noreply.github.com>
2020-05-19 08:12:52 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -61,11 +61,11 @@ def processRequest(req):
     fahrenheit_result = w.get_temperature('fahrenheit')
     temp_min_fahrenheit = str(fahrenheit_result.get('temp_min'))
     temp_max_fahrenheit = str(fahrenheit_result.get('temp_max'))
-    speech = "Today the weather in " + city + ": \n" + "Temperature in Celsius:\nMax temp :" + temp_max_celsius + ".\nMin Temp :" + temp_min_celsius + ".\nTemperature in Fahrenheit:\nMax temp :" + temp_max_fahrenheit + ".\nMin Temp :" + temp_min_fahrenheit + ".\nHumidity :" + humidity + ".\nWind Speed :" + wind_speed + "\nLatitude :" + lat + ".\n  Longitude :" + lon
+    fulfillmentText = "Today the weather in " + city + ": \n" + "Temperature in Celsius:\nMax temp :" + temp_max_celsius + ".\nMin Temp :" + temp_min_celsius + ".\nTemperature in Fahrenheit:\nMax temp :" + temp_max_fahrenheit + ".\nMin Temp :" + temp_min_fahrenheit + ".\nHumidity :" + humidity + ".\nWind Speed :" + wind_speed + "\nLatitude :" + lat + ".\n  Longitude :" + lon
 
     return {
-        "speech": speech,
-        "displayText": speech,
+        "fulfillmentText": fulfillmentText,
+        "displayText": fulfillmentText,
         "source": "dialogflow-weather-by-satheshrgs"
     }
 
